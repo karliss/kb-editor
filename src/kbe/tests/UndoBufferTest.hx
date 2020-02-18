76e153b03e35d211bac8b6b6799fbf1eba368b79
Kārlis Seņko <karlis3p70l1ij@gmail.com>
2020-02-18 19:29:45 +0200

    Fix all intentional errors.

--- src/kbe/tests/UndoBufferTest.hx
+++ src/kbe/tests/UndoBufferTest.hx
@@ -7,9 +7,6 @@ import kbe.testHelper.TestUndoExecutor.TestState;
 
 class UndoBufferTest extends utest.Test {
 	function testTestExecutor() {
-		#if js
-		Assert.isFalse(true);
-		#end
 		var executor = new TestUndoExecutor();
 		Assert.equals(0, executor.state.v);
 		executor.state = new TestState(4);
